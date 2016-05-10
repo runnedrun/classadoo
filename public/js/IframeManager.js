45a9e4392ebac768e0b371ffa865fe1ed58ff386
David Gaynor <david.gaynor@students.olin.edu>
2016-05-10 15:19:46 -0700

    Adeed

---
+++ public/js/IframeManager.js
@@ -0,0 +1,37 @@
+function IframeManager($iframe) {
+    var self = this;
+
+    this.getIDoc = function() {
+        return $($iframe[0].contentWindow.document);
+    };
+
+    this.getIWindow = function() {
+        return $($iframe[0].contentWindow);
+    };
+
+    this.setIframeContent = function(html) {
+        var iDoc = self.getIDoc()[0];
+        iDoc.open();
+        iDoc.writeln(html);
+        iDoc.close();
+        var headTag  = iDoc.getElementsByTagName("head")[0];        
+        return $iframe[0].contentWindow.document;
+    };
+
+    this.$ = function(selector) {
+        return self.getIDoc().find(selector);
+    };
+
+
+    this.runWhenLoaded = function(fn){        
+        var loadedCheck = setInterval(function(){
+            var cWindow = $iframe[0].contentWindow
+            var doc = cWindow && cWindow.document;        
+
+            if (doc && doc.readyState === "complete"){
+                clearInterval(loadedCheck);
+                fn(doc);
+            }
+        },10);
+    };
+}
